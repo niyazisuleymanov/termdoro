a951a710f1854a282e55ab819dc88109038b9598
Niyazi Suleymanov <niyazisuleymanov@protonmail.com>
2022-07-07 00:57:05 +0400

    add install_requires

--- setup.py
+++ setup.py
@@ -20,6 +20,14 @@ setuptools.setup(
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
   ],
+  install_requires=[
+    "arrow", 
+    "bullet", 
+    "click",
+    "pyfiglet",
+    "python-dateutil",
+    "six",
+  ],
   packages=['termdoro'],
   python_requires=">=3.6",
   entry_points={
